7e4c7d23ca3f630f0275f01eef417d2234253a8f
VitinhoReiDelas <vitor.buffon@etec.sp.gov.br>
2020-02-27 11:29:44 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+void main() => runApp(
+  MaterialApp(
+    home: Center(
+
+      child: Container(
+        color: Colors.red,
+        child: Text('Heloo Word!!', style: TextStyle(color: Colors.black) ),
+      ),
+    ),
+  ),
+);
